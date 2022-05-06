f4ea5aaf7f92f79cbc57ba4f19c83db0857bf794
Brian Campbell <bry123ftw@gmail.com>
2022-05-06 17:17:05 -0500

    delete button works

--- script.js
+++ script.js
@@ -7,35 +7,37 @@ function onReady() {
     console.log('JQ');
     //goes to the button click function
     $('#submit').on('click', submitButtonClick);
+    $('#tableBody').on('click', '.delete', deleteButtonClick);
+}
 
-    function submitButtonClick() {
-        //takes values from input
-        /*if ($('#firstName') === '' || $('#lastName') === '' || $('#id') ==='' || $('#title') === '' || $('#salary') === '' || $('#salary') === NaN){
-            alert("Please enter valid inputs!")
-            return;
-        }*/
+function submitButtonClick() {
+    //takes values from input
+    /*if ($('#firstName') === '' || $('#lastName') === '' || $('#id') ==='' || $('#title') === '' || $('#salary') === '' || $('#salary') === NaN){
+        alert("Please enter valid inputs!")
+        return;
+    }*/
 
-        let firstName = $('#firstName').val();
-        let lastName = $('#lastName').val();
-        let id = $('#id').val();                
-        let title = $('#title').val();
-        let salary = Number($('#salary').val()); //makes sure the input coming in is a number
-        total += salary; // updates total 
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let id = $('#id').val();
+    let title = $('#title').val();
+    let salary = Number($('#salary').val()); //makes sure the input coming in is a number
+    total += salary; // updates total 
 
-        let className = "";
-        if (total >= 20000){
-            className = 'high'
-        }
+    let className = "";
+    if (total >= 20000) {
+        className = 'high'
+    }
 
-        //emptying inputs
-        /*$('#firstName').val('');
-        $('#lastName').val('');
-        $('#id').val('');
-        $('#title').val('');
-        $('#salary').val('');*/
+    //emptying inputs
+    /*$('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#title').val('');
+    $('#salary').val('');*/
 
-        //updates table
-        $('#tableBody').append(`
+    //updates table
+    $('#tableBody').append(`
             <tr>
                 <td>${firstName}</td>
                 <td>${lastName}</td>
@@ -46,10 +48,14 @@ function onReady() {
             </tr>
         `)
 
-        //console.log(`First name: ${firstName}, last ${lastName}, id${id}, ${title}, ${salary}, ${total}`)
+    //console.log(`First name: ${firstName}, last ${lastName}, id${id}, ${title}, ${salary}, ${total}`)
 
-        $('#total').empty(); //empties the 'label that has the monthly $'
-        $('#total').append(`<h3 class="${className}"> Total Monthly: ${total} </h3>`); // adds 'hi' to the emptied label
+    $('#total').empty(); //empties the 'label that has the monthly $'
+    $('#total').append(`<h3 class="${className}"> Total Monthly: ${total} </h3>`); // adds 'hi' to the emptied label
 
-    }
+}
+
+function deleteButtonClick(event){
+    //deletes the row
+    $(event.target).closest('tr').remove();
 }
