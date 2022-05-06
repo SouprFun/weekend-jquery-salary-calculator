f1ea6988cdf8f0ab374899c2513f1802321ee14b
Brian Campbell <bry123ftw@gmail.com>
2022-05-06 16:12:49 -0500

    button click does something, added variables total doesnt work yet

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ console.log('JS');
 
 $(onReady)
 
-function onReady(){
+function onReady(){ 
     console.log('JQ');
+    //goes to the button click function
+    $('#submit').on('click', submitButtonClick); 
+
+    function submitButtonClick(){
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let id = $('#id').val();
+        let title = $('#title').val();
+        let salary = Number($('#salary').val());
+        let total = 0
+        total += salary;
+        
+
+        console.log(`First name: ${firstName}, last ${lastName}, id${id}, ${title}, ${salary}, ${total}`)
+
+        $('#total').empty(); //empties the 'label that has the monthly $'
+        $('#total').append(`Total Monthly: ${total}`); // adds 'hi' to the emptied label
+
+    }
 }
