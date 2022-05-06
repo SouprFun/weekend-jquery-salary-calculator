bba1561830c3c5de1d2f2f8ab11d2d79530bdf89
Brian Campbell <bry123ftw@gmail.com>
2022-05-06 16:48:59 -0500

    table is updating

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-console.log('JS'); 
+console.log('JS');
 
 $(onReady)
 let total = 0
 
-function onReady(){ 
+function onReady() {
     console.log('JQ');
     //goes to the button click function
-    $('#submit').on('click', submitButtonClick); 
+    $('#submit').on('click', submitButtonClick);
 
-    function submitButtonClick(){
+    function submitButtonClick() {
+        //takes values from input
+        /*if ($('#firstName') === '' || $('#lastName') === '' || $('#id') ==='' || $('#title') === '' || $('#salary') === '' || $('#salary') === NaN){
+            alert("Please enter valid inputs!")
+            return;
+        }*/
+
         let firstName = $('#firstName').val();
         let lastName = $('#lastName').val();
-        let id = $('#id').val();            //takes values from input
+        let id = $('#id').val();                
         let title = $('#title').val();
-        let salary = Number($('#salary').val());
-        total += salary;
-        
+        let salary = Number($('#salary').val()); //makes sure the input coming in is a number
+        total += salary; // updates total 
+
+        //emptying inputs
+        /*$('#firstName').val('');
+        $('#lastName').val('');
+        $('#id').val('');
+        $('#title').val('');
+        $('#salary').val('');*/
+
+        $('#tableBody').append(`
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${id}</td>
+                <td>${title}</td>
+                <td>${salary}</td>
+            </tr>
+        `)
 
         console.log(`First name: ${firstName}, last ${lastName}, id${id}, ${title}, ${salary}, ${total}`)
 
